47c8c35b43060364308f025f625f077ee8d4ea6e
akshayAppd <akshay.srivastava@appdynamics.com>
2018-02-19 19:59:21 +0530

    v1.0.1 - Updated Licenses

--- src/main/java/com/appdynamics/monitors/gce/GCEMonitor.java
+++ src/main/java/com/appdynamics/monitors/gce/GCEMonitor.java
@@ -1,4 +1,11 @@
 
+/*
+ * Copyright 2014. AppDynamics LLC and its affiliates.
+ *  All Rights Reserved.
+ *  This is unpublished proprietary source code of AppDynamics LLC and its affiliates.
+ *  The copyright notice above does not evidence any actual or intended publication of such source code.
+ */
+
 package com.appdynamics.monitors.gce;
 
 import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
